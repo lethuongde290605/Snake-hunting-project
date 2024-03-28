cef3a3e4b785e8479ba8564f64d5abc1db36e85b
lethuongde290605 <lethuongde290605@gmail.com>
2024-03-28 14:27:33 +0700

    Upload snake hunting

---
+++ snake.cpp
@@ -0,0 +1,20 @@
+﻿#include "Header.h"
+#include "Level.h"
+#include "Menu.h"
+#include "Header.h"
+#include "SetUp.h"
+#include <thread>
+
+int main() {
+	std::thread inputThread(Input);
+	std::thread soundThread(Sound);
+	FixConsoleWindow();
+	SetConsoleWindow(1280, 720);
+	MoveCenter();
+	HideCursor();
+	menu();
+	inputThread.join();
+	soundThread.join();
+
+	return 0;
+}
